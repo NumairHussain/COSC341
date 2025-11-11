1af6a6c5802405e478f51db882129c5c3a8f0bb8
NumairHussain <123851476+NumairHussain@users.noreply.github.com>
2025-11-11 13:26:11 -0500

    started scores question

--- c++/program.cpp
+++ c++/program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 double compute_pi(int n)
 {
@@ -14,6 +15,20 @@ double compute_pi(int n)
     return 4 * pi;
 }
 
+void scores()
+{
+    while(true)
+    {
+        std::string input;
+        std::string exit_case = "-1";
+        std::cin >> input;
+        if (input == exit_case)
+        {
+            break;
+        }
+        
+    }
+}
 
 
 int main()
