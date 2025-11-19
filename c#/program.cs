605fcc25f1d416fcc148bcb8826d577309e95495
NumairHussain <123851476+NumairHussain@users.noreply.github.com>
2025-11-18 21:47:46 -0500

    finished CreateIDPassword

--- c#/program.cs
+++ c#/program.cs
@@ -48,3 +48,27 @@ void scores()
         Console.WriteLine("No scores entered");
     }
 }
+
+void createIDPassword(string last, string first)
+{
+    id = first[0] + last;
+    password = first[0] + first[first.Length - 1] + last.Substring(0, 3) + first.Length + "" + last.Length;
+    Console.WriteLine("ID: " + id.ToUpper());
+    Console.WriteLine("Password: " + password.ToUpper());
+}
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        // Call the scores function
+        scores();
+        
+        // Create ID and password
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter first name: ");
+        string firstName = Console.ReadLine();
+        createIDPassword(lastName, firstName);
+    }
+}
