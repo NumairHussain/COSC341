804421f472278d6d41085bce4cf659ea78bcecc0
NumairHussain <123851476+NumairHussain@users.noreply.github.com>
2025-11-18 22:01:14 -0500

    finished file_sort

--- c#/program.cs
+++ c#/program.cs
@@ -57,6 +57,66 @@ void createIDPassword(string last, string first)
     Console.WriteLine("Password: " + password.ToUpper());
 }
 
+void fileSort(string infile, string outfile)
+{
+    // Read student data from input file
+    StreamReader fin = new StreamReader(infile);
+    int numStudents = 0;
+    numStudents = Convert.ToInt32(fin.ReadLine());
+
+    // Arrays to hold student data
+    int[] studentIds = new int[numStudents];
+    char[] studentGrades = new char[numStudents];
+    double[] studentGpas = new double[numStudents];
+
+    // Read student data from file
+    for (int i = 0; i < numStudents; i++)
+    {
+        string line = fin.ReadLine();
+        string[] parts = line.Split(' ');
+        studentIds[i] = Convert.ToInt32(parts[0]);
+        studentGrades[i] = Convert.ToChar(parts[1]);
+        studentGpas[i] = Convert.ToDouble(parts[2]);
+        Console.WriteLine(studentIds[i] + " " + studentGrades[i] + " " + studentGpas[i]);
+    }
+
+    fin.Close();
+
+    // Sort students by ID using bubble sort
+    for (int i = 0; i < numStudents; i++)
+    {
+        for (int j = 0; j < numStudents - i - 1; j++)
+        {
+            if (studentIds[j] > studentIds[j + 1])
+            {
+                // Swap student data
+                int tempId = studentIds[j];
+                studentIds[j] = studentIds[j + 1];
+                studentIds[j + 1] = tempId;
+
+                // Swap grades
+                char tempGrade = studentGrades[j];
+                studentGrades[j] = studentGrades[j + 1];
+                studentGrades[j + 1] = tempGrade;
+
+                // Swap gpas
+                double tempGpa = studentGpas[j];
+                studentGpas[j] = studentGpas[j + 1];
+                studentGpas[j + 1] = tempGpa;
+            }
+        }
+    }
+
+    // Write sorted data to output file
+    StreamWriter fout = new StreamWriter(outfile);
+    fout.WriteLine(numStudents);
+    for (int i = 0; i < numStudents; i++)
+    {
+        fout.WriteLine(studentIds[i] + " " + studentGrades[i] + " " + studentGpas[i].ToString("F2"));
+    }
+    fout.Close();
+}
+
 class program
 {
     public static void Main(string[] args)
